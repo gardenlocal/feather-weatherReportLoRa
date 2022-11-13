d63359cec5001050b031cf5074e674e54769eaa0
Yi donghoon <icq4ever@gmail.com>
2022-11-14 02:01:52 +0900

    auto, interval, spray, run. timer added

--- featherFirmware/featherFirmware.ino
+++ featherFirmware/featherFirmware.ino
@@ -35,6 +35,16 @@ const int RFM95_RST   = 4;
 const int RFM95_INT   = 7;
 const float RF95_FREQ = 900.0;
 
+// automode, spray(seconds), interval(minutes), run in manual mode
+boolean autoMode = true;
+int spray = 5;        // spray for seconds. default : 5 seconds
+int interval = 60;    // spray interval. default: 60 minutes
+boolean run = false;  // run in manual.  default = false
+
+// timer for spray
+uint64_t tLastIntervalCheck;
+uint64_t tLastSprayStart;
+
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
 // DHT setup
@@ -45,15 +55,15 @@ uint64_t timerReadSensor;
 
 // 32bit float share memory to 4byte bin
 typedef union {
-  float numFloat;
-  byte numBin[4];
+	float numFloat;
+	byte numBin[4];
 } floatingNumber;
 
 
 // 16 bit uint16_t share memory with 2byte bin
 typedef union {
-  uint16_t numBigInt;
-  byte numBin[2];
+	uint16_t numBigInt;
+	byte numBin[2];
 } uint16Number;
 
 floatingNumber temperature, humidity;
@@ -62,180 +72,198 @@ bool isCharging = false;
 bool bFogOn = false;
 
 void pinSetup() {
-  pinMode(PIN_LED, OUTPUT);
-  pinMode(PIN_CHG, INPUT);
-  pinMode(PIN_GOOD, INPUT);
-  pinMode(PIN_SOLENOID, OUTPUT);
-  pinMode(PIN_LED_SOLENOID, OUTPUT);
-  pinMode(PIN_LED_GOOD, OUTPUT);
-  pinMode(PIN_DHT, INPUT);
+	pinMode(PIN_LED, OUTPUT);
+	pinMode(PIN_CHG, INPUT);
+	pinMode(PIN_GOOD, INPUT);
+	pinMode(PIN_SOLENOID, OUTPUT);
+	pinMode(PIN_LED_SOLENOID, OUTPUT);
+	pinMode(PIN_LED_GOOD, OUTPUT);
+	pinMode(PIN_DHT, INPUT);
 //  pinMode(PIN_SOIL_VCC, OUTPUT);
 //  pinMode(PIN_SOIL_READ, OUTPUT);
 
-  digitalWrite(PIN_LED, LOW);
+	digitalWrite(PIN_LED, LOW);
 }
 
 
 // print sensor value to serial monitor
 void printWeatherData() {
-  Serial.print("Temperature:");
-  Serial.print(temperature.numFloat);
-  Serial.print("c, \t");
-  Serial.print("Humidity:");
-  Serial.print(humidity.numFloat);
-  Serial.println("% \t");
-  Serial.print("SOIL :");
-  Serial.println(soil.numBigInt);
+	Serial.print("Temperature:");
+	Serial.print(temperature.numFloat);
+	Serial.print("c, \t");
+	Serial.print("Humidity:");
+	Serial.print(humidity.numFloat);
+	Serial.println("% \t");
+	Serial.print("SOIL :");
+	Serial.println(soil.numBigInt);
 }
 
 void getSoilData() {
 //  digitalWrite(PIN_SOIL_VCC, HIGH);
-    soil.numBigInt = analogRead(PIN_SOIL_READ);
+	soil.numBigInt = analogRead(PIN_SOIL_READ);
 //  soil.numBigInt = millis();
 
 //  digitalWrite(PIN_SOIL_VCC, LOW);
 }
 
 void receivecMessage() {
-  if (rf95.available()) {
-    uint8_t recvBufferLen = sizeof(recvBuffer);
+	if (rf95.available()) {
+		uint8_t recvBufferLen = sizeof(recvBuffer);
 
-    if (rf95.recv(recvBuffer, &recvBufferLen)) {
-      digitalWrite(PIN_LED, HIGH);
+		if (rf95.recv(recvBuffer, &recvBufferLen)) {
+			digitalWrite(PIN_LED, HIGH);
 
-      if (recvBuffer[0] == '/') {
-        if (recvBuffer[1] == DEVICE_ID) {   // only receiving device id
-          if (recvBuffer[2] == 'F') {
-            if (recvBuffer[3] == '1') bFogOn = true;
-            else                      bFogOn = false;
-          }
-        }
-      } else {  // if device id is not mine, quit function
-        return;
-      }
-    }
+			if (recvBuffer[0] == '/') {
+				if (recvBuffer[1] == DEVICE_ID) {   // only receiving device id
+					if (recvBuffer[2] == 'F') {
+						if (recvBuffer[3] == '1') run = true;
+						else                      run = false;
+					}
+				}
+			} else {  // if device id is not mine, quit function
+				return;
+			}
+		}
 
-    digitalWrite(PIN_LED, LOW);
-  } else {
-    // not recv
-  }
+		digitalWrite(PIN_LED, LOW);
+	} else {
+		// not recv
+	}
 }
 
 void sendMessage() {
-  uint8_t reply[15];
-  reply[0] = '/';
-  reply[1] = 'R'; // report packet
-  reply[2] = DEVICE_ID;               // device ID. int? byte? hmm...
-  reply[3] = temperature.numBin[0];   // 32bit float bin
-  reply[4] = temperature.numBin[1];
-  reply[5] = temperature.numBin[2];
-  reply[6] = temperature.numBin[3];
-  reply[7] = humidity.numBin[0];      // 32bit float bin
-  reply[8] = humidity.numBin[1];
-  reply[9] = humidity.numBin[2];
-  reply[10] = humidity.numBin[3];
-  reply[11] = soil.numBin[0];         // 16bit int bin
-  reply[12] = soil.numBin[1];
-  if (isCharging) reply[13] = 1;      // chargning status bin
-  else            reply[13] = 0;
-  reply[14] = 0;
+	uint8_t reply[15];
+	reply[0] = '/';
+	reply[1] = 'R'; // report packet
+	reply[2] = DEVICE_ID;               // device ID. int? byte? hmm...
+	reply[3] = temperature.numBin[0];   // 32bit float bin
+	reply[4] = temperature.numBin[1];
+	reply[5] = temperature.numBin[2];
+	reply[6] = temperature.numBin[3];
+	reply[7] = humidity.numBin[0];      // 32bit float bin
+	reply[8] = humidity.numBin[1];
+	reply[9] = humidity.numBin[2];
+	reply[10] = humidity.numBin[3];
+	reply[11] = soil.numBin[0];         // 16bit int bin
+	reply[12] = soil.numBin[1];
+	if (isCharging) reply[13] = 1;      // chargning status bin
+	else            reply[13] = 0;
+	reply[14] = 0;
 
-  rf95.send(reply, sizeof(reply));
-  digitalWrite(PIN_LED, LOW);
-  //  Serial.println("weatherData Sent!");
-  printWeatherData();
+	rf95.send(reply, sizeof(reply));
+	digitalWrite(PIN_LED, LOW);
+	//  Serial.println("weatherData Sent!");
+	printWeatherData();
 }
 
 
 void setup() {
-  Serial.begin(115200);
+	Serial.begin(115200);
 
-  pinSetup();
+	pinSetup();
 
-  temperature.numFloat = 0.f;
-  humidity.numFloat = 0.f;
-  soil.numBigInt = 0;
+	temperature.numFloat = 0.f;
+	humidity.numFloat = 0.f;
+	soil.numBigInt = 0;
 
-  delay(1000);
-  Serial.println("BOOTING....");
+	delay(1000);
+	Serial.println("BOOTING....");
 
-  dht.begin();
-  initLoRa();     // init LoRa
-  timerReadSensor = millis(); // init timer stamp
+	dht.begin();
+	initLoRa();     // init LoRa
+	timerReadSensor = millis(); // init timer stamp
 }
 
 void loop() {
-  uint16_t error;
-  char errorMessage[256];
+	uint16_t error;
+	char errorMessage[256];
 
-  if (millis() - timerReadSensor > 5000) {
-    getSoilData();
-    getWeatherData();
-    printWeatherData();
+	if(millis() - tLastIntervalCheck > interval*60*1000){
+		Serial.println("interval checked!");
+		tLastSprayStart = millis();
+	}
 
-    sendMessage();          // send radio message
-    timerReadSensor = millis();
-    bFogOn = !bFogOn;
-  }
+	if(millis()-tLastSprayStart > spray*1000)	bFogOn = true;
+	else 										bFogOn = false;
 
-  // checking batt status and LED out
-  if (digitalRead(PIN_GOOD)) digitalWrite(PIN_LED_GOOD, LOW);
-  else                      digitalWrite(PIN_LED_GOOD, HIGH);
+	if (millis() - timerReadSensor > 5000) {
+		getSoilData();
+		getWeatherData();
+		printWeatherData();
 
-  if(digitalRead(PIN_CHG))  isCharging = false;
-  else                      isCharging = true;
+		sendMessage();          // send radio message
+		timerReadSensor = millis();
+		bFogOn = !bFogOn;
+	}
+
+	// checking batt status and LED out
+	if (digitalRead(PIN_GOOD)) 	digitalWrite(PIN_LED_GOOD, LOW);
+	else                      	digitalWrite(PIN_LED_GOOD, HIGH);
+
+	if(digitalRead(PIN_CHG))  isCharging = false;
+	else                      isCharging = true;
 
 
-  // fog control
-  if (bFogOn)    {
-    digitalWrite(PIN_SOLENOID, HIGH);
-    digitalWrite(PIN_LED_SOLENOID, HIGH);
-  }
-  else {
-    digitalWrite(PIN_SOLENOID, LOW);
-    digitalWrite(PIN_LED_SOLENOID, LOW);
-  }
-
-  //  Serial.print(digitalRead(PIN_GOOD));
-  //  Serial.print(" - ");
-  //  Serial.println(digitalRead(PIN_CHG));
+	// fog control
+	if(autoMode){
+		if (bFogOn)    {
+			digitalWrite(PIN_SOLENOID, HIGH);
+			digitalWrite(PIN_LED_SOLENOID, HIGH);
+			Serial.println("Fog ON!");
+		}
+		else {
+			digitalWrite(PIN_SOLENOID, LOW);
+			digitalWrite(PIN_LED_SOLENOID, LOW);
+			Serial.println("Fog OFF");
+		}
+	} else {	// manual mode
+		if(run){
+			digitalWrite(PIN_SOLENOID, HIGH);
+			digitalWrite(PIN_LED_SOLENOID, HIGH);
+			Serial.println("Fog ON!");
+		} else {
+			digitalWrite(PIN_SOLENOID, LOW);
+			digitalWrite(PIN_LED_SOLENOID, LOW);
+			Serial.println("Fog OFF");
+		}
+	}
 }
 
 void initLoRa() { // init
-  pinMode(RFM95_RST, OUTPUT);
-  digitalWrite(RFM95_RST, HIGH);
+	pinMode(RFM95_RST, OUTPUT);
+	digitalWrite(RFM95_RST, HIGH);
 
-  Serial.println("Feather LoRa RX Test!");
+	Serial.println("Feather LoRa RX Test!");
 
-  // manual LoRa rest
-  digitalWrite(RFM95_RST, LOW);
-  delay(10);
-  digitalWrite(RFM95_RST, HIGH);
-  delay(10);
+	// manual LoRa rest
+	digitalWrite(RFM95_RST, LOW);
+	delay(10);
+	digitalWrite(RFM95_RST, HIGH);
+	delay(10);
 
-  while (!rf95.init()) {
-    Serial.println("LoRa radio init failed");
-    while (1);
-  }
+	while (!rf95.init()) {
+		Serial.println("LoRa radio init failed");
+		while (1);
+	}
 
-  Serial.println("LoRa radio init OK!");
+	Serial.println("LoRa radio init OK!");
 
-  if (!rf95.setFrequency(RF95_FREQ)) {
-    Serial.println("setFrequency failed.");
-    while (1);
-  }
+	if (!rf95.setFrequency(RF95_FREQ)) {
+		Serial.println("setFrequency failed.");
+		while (1);
+	}
 
-  Serial.print("Set Freq to : ");
-  Serial.println(RF95_FREQ);
+	Serial.print("Set Freq to : ");
+	Serial.println(RF95_FREQ);
 
-  // default transmitter power is 13dbm, using PA_BOOST
-  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
-  // you can set transmitter powers from 5 to 23 dBm:
-  rf95.setTxPower(23, false);
+	// default transmitter power is 13dbm, using PA_BOOST
+	// If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
+	// you can set transmitter powers from 5 to 23 dBm:
+	rf95.setTxPower(23, false);
 }
 
 void getWeatherData() {
-  // dump demo
-  temperature.numFloat = dht.readTemperature();
-  humidity.numFloat = dht.readHumidity();
+	// dump demo
+	temperature.numFloat = dht.readTemperature();
+	humidity.numFloat = dht.readHumidity();
 }
